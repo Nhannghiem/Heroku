28905345af5462f0ac3ad2f4382b10cb9706d84f
Nhannghiem <87642041+Nhannghiem@users.noreply.github.com>
2021-08-09 11:36:47 +0700

    ok

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=> {
-    res.sendFile(__dirname + '/view/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 })
 
 const PORT = process.env.PORT ||5000;
 app.listen(PORT);
-console.log('sever is running:', PORT)
+console.log('sever is running:', PORT);
